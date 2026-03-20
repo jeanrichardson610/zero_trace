0d104a99b1972564b38b9e41e4c27aab4c409e72
jeanrichardson610 <jean.richardson610@gmail.com>
2026-03-19 20:51:33 -0700

    testing

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,10 @@
-import { treaty } from "@elysiajs/eden"
-import type { App } from "../app/api/[[...slugs]]/route"
+// client.ts
+import { treaty } from "@elysiajs/eden";
+import type { App } from "../app/api/[[...slugs]]/route";
 
-export const client = treaty<App>("localhost:3000").api
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000" // local dev
+    : "https://zero-trace-six.vercel.app/"; 
+
+export const client = treaty<App>(BASE_URL).api;
